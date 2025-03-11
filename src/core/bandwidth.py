51c4496540971f4860c5a97eb5d9a71d52a6ddc0
Shaik Ali <89526384+ShaikAli65@users.noreply.github.com>
2025-03-11 13:09:16 +0530

    added a maintenance routine that checks for any invalid sockets periodically

--- src/core/bandwidth.py
+++ src/core/bandwidth.py
@@ -1,13 +1,9 @@
+import asyncio
 from collections import defaultdict
 
-from src.avails import RemotePeer
+from src.avails import RemotePeer, const, use
 from src.avails.connect import Connection, Socket, is_socket_connected
 from src.avails.mixins import AExitStackMixIn, singleton_mixin
-from src.core import public
-
-
-async def start_watcher():
-    await public.Dock.exit_stack.enter_async_context(Watcher())
 
 
 @singleton_mixin
@@ -20,24 +16,33 @@ class Watcher(AExitStackMixIn):
                 Connection,
                 Socket,
             ],
-        ] = defaultdict(dict)  # connection related to remotepeer keyed with connection tuple and valued with raw socket
+        ] = defaultdict(
+            dict)  # connection related to remotepeer keyed and valued with connection tuple and raw socket respectively
+        self._total_socks = 0
+        self._maintenance_task = asyncio.Task(self._maintenance())
 
     def watch(self, socket: Socket, connection: Connection):
         self.sockets[connection.peer][connection] = socket
         self._exit_stack.enter_context(socket)
+        self._total_socks += 1
 
     async def refresh(self, peer, *connections: Connection):
+        connections = set(connections)
         active = set()
         not_active = set()
-        for conn, sock in tuple(self.sockets[peer].items()):
+
+        for conn, sock in self.sockets[peer].items():
             if conn not in connections:
                 continue
             if is_socket_connected(sock):
                 active.add(conn)
             else:
-                self.sockets[peer].pop(conn)
                 sock.close()  # RIP
                 not_active.add(conn)
+
+        for conn in not_active:
+            self.sockets[peer].pop(conn)
+            self._total_socks -= 1
 
         return active, not_active
 
@@ -49,3 +54,34 @@ class Watcher(AExitStackMixIn):
 
         """
         await self.refresh(peer, *self.sockets[peer].keys())
+
+    async def _maintenance(self):
+        while True:
+            await asyncio.sleep(1)
+            if self.total_connections < const.MAX_TOTAL_CONNECTIONS:
+                continue
+
+            to_be_removed = []
+
+            for peer, conns in self.sockets.items():
+                for conn, sock in conns.items():
+                    last_accessed = max(conn.send.last_updated_time, conn.recv.last_updated_time)
+                    if last_accessed >= const.MAX_IDLE_TIME_FOR_CONN:
+                        sock.close()
+                        to_be_removed.append((peer, conn))
+
+            for peer, conn in to_be_removed:
+                self.sockets[peer].pop(conn)
+
+    async def request_closing(self, conn: Connection):
+        self.sockets[conn.peer][conn].close()
+
+    @property
+    def total_connections(self):
+        return self._total_socks
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await use.safe_cancel_task(self._maintenance_task)
